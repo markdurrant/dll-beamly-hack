23a24dfabdc751ec85e9d2c14b7a2e5dd7a0ea72
Mark Durrant <mark@pebblecode.com>
2014-05-11 12:24:30 +0100

    absolute postion all the things!

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,22 @@
 $( document ).ready( function() {
   
   $( '.channel-item' ).click( function( event ) {
-    $( '.active' ).toggleClass( 'active' );
+    $( '.channel-item.active' ).toggleClass( 'active' );
     $( this ).addClass( 'active' );
     $( '.quote-btn-container' ).removeClass( 'hidden' );
+  });
+
+  $( '.caption-chunk' ).click( function( event ) {
+    $( this ).toggleClass( 'active' );
+
+    $( '.share-btn-container' ).addClass('active');
+  });
+
+  $('.caption-list').scrollTop($('.caption-list').scrollHeight);
+
+  $( '.quote-btn' ).click( function( event ) {
+    $( '.page-1' ).removeClass( 'active' );
+    $( '.page-2' ).addClass( 'active' );
   });
 
 });
